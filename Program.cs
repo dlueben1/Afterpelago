f67ec035793ba588153f8ad15a543cda212cbc9c
Douglas Lueben <dlueben1@umbc.edu>
2025-11-01 14:43:54 -0400

    build: install mudblazor

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace Afterpelago
 {
@@ -12,6 +13,8 @@ namespace Afterpelago
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
