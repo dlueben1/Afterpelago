798ee6d153b04bd9cc81059d7d78b1cabbaf4a67
Douglas Lueben <dlueben1@umbc.edu>
2026-03-01 13:14:05 -0500

    feat: basic dashboard

---
+++ Connectors/Archipelago/LogImporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfterpelagoWPF.Connectors
+{
+    /// <summary>
+    /// Imports session data from an Archipelago Log File.
+    /// </summary>
+    public static class LogImporter
+    {
+
+    }
+}
